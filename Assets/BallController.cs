7150246a278a994af8b7c112794b22ceeccd586a
dylanstephenalexander <dylanstephenalexander@gmail.com>
2025-01-28 16:03:02 -0800

    initialized empty project

---
+++ Assets/BallController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody sphereRigidbody;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Calling the Start method.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 inputVector = Vector2.zero;
+       if(Input.GetKey(KeyCode.W)){
+        inputVector += Vector2.up;
+       }
+
+        if(Input.GetKey(KeyCode.S)){
+        inputVector += Vector2.down;
+       }
+
+        if(Input.GetKey(KeyCode.D)){
+        inputVector += Vector2.right;
+       }
+
+        if(Input.GetKey(KeyCode.A)){
+        inputVector += Vector2.left;
+       }
+       Debug.Log("Resulting Vector: " + inputVector);
+       Vector3 inputXZPlane = new Vector3(inputVector.x,0,inputVector.y);
+       sphereRigidbody.AddForce(inputXZPlane);
+       Debug.Log("Resulting 3D Vector: " + inputXZPlane);
+       
+    }
+}
